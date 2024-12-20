40a5cc3e81a9e75aa823e0712e0d85e525bd2ac1
M397 <m397.dev@gmail.com>
2024-12-20 11:00:38 +0700

    Initial

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+/**
+ * Class BaseController.
+ *
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ *
+ * Extend this class in any new controllers:
+ *
+ * ```
+ *     class Home extends BaseController
+ * ```
+ *
+ * For security be sure to declare any new methods as protected or private.
+ */
+abstract class BaseController
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
